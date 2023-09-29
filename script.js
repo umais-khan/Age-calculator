33845bf6e4e533842f3639cf94aba2ef20655d41
Fahad Ali <134847376+FaddyWho@users.noreply.github.com>
2023-09-29 10:02:25 +0500

    html ids added

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+
+
+const currentDay = 29;
+const currentMonth = 9;
+const currentYear = 2023;
+
+const dayEl = document.getElementById("user-day")
+const monthEl = document.getElementById("user-month")
+const yearEl = document.getElementById("user-year")
+const buttonEl = document.getElementById("submit-button")
+
+buttonEl.addEventListener('click', function() {
+    let dayValue = dayEl.value;
+    let monthValue = monthEl.value;
+    let yearValue = yearEl.value;
+
+    
+})
+
+
+
