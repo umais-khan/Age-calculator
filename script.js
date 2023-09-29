aa2dde69cf87fd7cc1dc2a5de297397d144e0340
unknown <umaisk23@gmail.com>
2023-09-29 17:15:39 +0100

    done

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 // setting up the current date into variables
 const mydate = new Date('2023-05-20'),
-    currentDay = mydate.getDate(),       
-    currentMonth = mydate.getMonth() +1,   
-    currentYear = mydate.getFullYear();    
+    currentDay = mydate.getDate(),
+    currentMonth = mydate.getMonth() + 1,
+    currentYear = mydate.getFullYear();
 
 // setting a list that defines how many days are in the particular months
 const monthList = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
@@ -14,24 +14,24 @@ const monthEl = document.getElementById("user-month")
 const yearEl = document.getElementById("user-year")
 
 const buttonEl = document.getElementById("submit-button")
-const errorEL= document.getElementById("error")
+const errorEL = document.getElementById("error")
 
-const outYear =document.getElementById("year-output")
-const outMonth =document.getElementById("month-output")
-const outDay =document.getElementById("day-output")
+const outYear = document.getElementById("year-output")
+const outMonth = document.getElementById("month-output")
+const outDay = document.getElementById("day-output")
 
 
 //event listener to get and calculate the difference
 
-buttonEl.addEventListener('click', function(e) {
+buttonEl.addEventListener('click', function (e) {
     //form validation
     let messages = [];
-    
-    if (dayEl.value === '' || monthEl.value === '' || yearEl.value === '' ) {
+
+    if (dayEl.value === '' || monthEl.value === '' || yearEl.value === '') {
         messages.push('DOB is required')
     }
 
-    if (yearEl.value > currentYear){
+    if (yearEl.value > currentYear) {
         messages.push("so you're a time traveler huh?")
     }
 
@@ -48,7 +48,7 @@ buttonEl.addEventListener('click', function(e) {
 
     //variables for the output age
     let monthOut;
-    let yearOut; 
+    let yearOut;
     let dayOut;
 
     //variable for the user input DOB
@@ -59,21 +59,21 @@ buttonEl.addEventListener('click', function(e) {
 
     //calculate the difference
 
-    if(currentMonth -monthValue < 0){
-        monthOut =  currentMonth - monthValue +12
-        yearOut = currentYear-yearValue-1
-    } 
+    if (currentMonth - monthValue < 0) {
+        monthOut = currentMonth - monthValue + 12
+        yearOut = currentYear - yearValue - 1
+    }
     else {
         monthOut = currentMonth - monthValue;
-        yearOut =  currentYear-yearValue
+        yearOut = currentYear - yearValue
     }
 
-    if(currentDay -dayValue <0){
-        monthOut = monthOut-1
-        dayOut= currentDay - dayValue + monthList[monthOut+1];
-        } else {
+    if (currentDay - dayValue < 0) {
+        monthOut = monthOut - 1
+        dayOut = currentDay - dayValue + monthList[monthOut + 1];
+    } else {
         dayOut = currentDay - dayValue;
-        }
+    }
 
     // show the output age
 
@@ -84,6 +84,37 @@ buttonEl.addEventListener('click', function(e) {
     outDay.innerHTML = `${dayOut} days`
 
 })
+// working of dark mode
+function dark_mode() {
+    var body = document.querySelector("body");
+    var container = document.querySelector(".container");
+    var labels = document.querySelector(".labels");
+    var inputs = document.querySelectorAll("input");
+    var button = document.querySelector(".dark-mode"); // Assuming there is only one button with the class "dark-mode"
 
-
-
+    if (body.style.backgroundColor === "black") {
+        // If already in dark mode, switch back to light mode
+        body.style.backgroundColor = "white";
+        container.style.backgroundColor = "white";
+        labels.style.color = "black";
+        if (button) {
+            button.style.backgroundColor = "black";
+            button.style.color = "white";
+        }
+        inputs.forEach(function (input) {
+            input.style.color = "black";
+        });
+    } else {
+        // Switch to dark mode
+        body.style.backgroundColor = "black";
+        container.style.backgroundColor = "black";
+        labels.style.color = "white";
+        if (button) {
+            button.style.backgroundColor = "white";
+            button.style.color = "black";
+        }
+        inputs.forEach(function (input) {
+            input.style.color = "white";
+        });
+    }
+}
